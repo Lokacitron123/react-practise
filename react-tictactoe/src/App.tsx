4cbebaf364b54c70071496ac578cc716eac1a489
Johan Lindell <johan.lindell91@gmail.com>
2026-05-17 12:05:16 +0200

    Lifted gameboard state logic to App

--- react-tictactoe/src/App.tsx
+++ react-tictactoe/src/App.tsx
@@ -1,12 +1,29 @@
 import { useState } from "react";
 import GameBoard from "./components/GameBoard";
 import Player from "./components/Player";
+import Log from "./components/Log";
 
 export type PlayerSymbol = "X" | "O";
+
+export type CellValue = "X" | "O" | null;
+
+// Tuple array with fixed values
+export type GameBoardType = [
+  [CellValue, CellValue, CellValue],
+  [CellValue, CellValue, CellValue],
+  [CellValue, CellValue, CellValue],
+];
+
+const initialGameBoard: GameBoardType = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
 
 function App() {
   // Union type to select which player's turn it is
   const [activePlayer, setActivePlayer] = useState<PlayerSymbol>("X");
+  const [gameBoard, setGameBoard] = useState<GameBoardType>(initialGameBoard);
 
   return (
     <main>
@@ -27,9 +44,11 @@ function App() {
         <GameBoard
           activePlayer={activePlayer}
           setActivePlayer={setActivePlayer}
+          gameBoard={gameBoard}
+          setGameBoard={setGameBoard}
         />
       </div>
-      Log
+      <Log />
     </main>
   );
 }
--- react-tictactoe/src/components/GameBoard.tsx
+++ react-tictactoe/src/components/GameBoard.tsx
@@ -1,29 +1,18 @@
-import { useState } from "react";
-import type { PlayerSymbol } from "../App";
+import type { GameBoardType, PlayerSymbol } from "../App";
 
 type GameBoardProps = {
   activePlayer: PlayerSymbol;
   setActivePlayer: React.Dispatch<React.SetStateAction<PlayerSymbol>>;
+  gameBoard: GameBoardType;
+  setGameBoard: React.Dispatch<React.SetStateAction<GameBoardType>>;
 };
 
-type CellValue = "X" | "O" | null;
-
-// Tuple array with fixed values
-type GameBoardType = [
-  [CellValue, CellValue, CellValue],
-  [CellValue, CellValue, CellValue],
-  [CellValue, CellValue, CellValue],
-];
-
-const initialGameBoard: GameBoardType = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null],
-];
-
-const GameBoard = ({ activePlayer, setActivePlayer }: GameBoardProps) => {
-  const [gameBoard, setGameBoard] = useState<GameBoardType>(initialGameBoard);
-
+const GameBoard = ({
+  activePlayer,
+  setActivePlayer,
+  gameBoard,
+  setGameBoard,
+}: GameBoardProps) => {
   // Update object & arrays in an immutable way. E.g. make a (deep) copy first is good practice
   const handleSelectSquare = (rowIndex: number, colIndex: number) => {
     setGameBoard((prevGameBoard) => {
